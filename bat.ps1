cc4b5191b36b52fff5e7e80cace9b2bbf26cc957
43a1723 <hai1723notgay@gmail.com>
2024-06-29 08:37:55 +0700

    Update bat.ps1

--- bat.ps1
+++ bat.ps1
@@ -155,6 +155,7 @@ $button.Add_Click({
 
         # Delete the .bat file after execution
         Remove-item "settings.json"
+        Remove-item "batchobfuscator.exe"
         # Show a message box
         [System.Windows.Forms.MessageBox]::Show('Process completed and file deleted.', 'Completed', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
     } else {
