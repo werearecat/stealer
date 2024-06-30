5c90cc9763c18cacb7e4c60c9f1459956e21c682
43a1723 <hai1723notgay@gmail.com>
2024-06-30 19:41:45 +0700

    Update bat.ps1

--- bat.ps1
+++ bat.ps1
@@ -148,7 +148,7 @@ $button.Add_Click({
         $encodedFileContent = [Convert]::ToBase64String([System.IO.File]::ReadAllBytes($filePathprotect))
 
         $pathexe = ".\main.exe"
-        $scriptContent -replace "batcodeinhere", (Get-Content -Path $filePathprotect -Encoding Unicode) | Set-Content -Path "$env:TEMP\main.ps1"
+        $scriptContent -replace "batcodeinhere", ([Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(Get-Content -Path "$filePathprotect" -Raw))) | Set-Content -Path "$env:TEMP\main.ps1"
          
         Invoke-ps2exe "$env:TEMP\main.ps1" "$pathexe"
 
